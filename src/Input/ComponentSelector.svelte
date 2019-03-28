4c87cc5aa783356af3da944d097cc5db8f57abfb
Richard Harris <richard.a.harris@gmail.com>
2019-03-28 10:09:30 -0400

    fix icons

--- src/Input/ComponentSelector.svelte
+++ src/Input/ComponentSelector.svelte
@@ -175,6 +175,25 @@
 	.add-new:hover {
 		color: var(--flash) !important;
 	}
+
+	svg {
+		position: relative;
+		/* width: 1.2em;
+		height: 1.2em;
+		top: -0.123em; */
+		overflow: hidden;
+		vertical-align: middle;
+		-o-object-fit: contain;
+		object-fit: contain;
+		-webkit-transform-origin: center center;
+		transform-origin: center center;
+
+		stroke: currentColor;
+		stroke-width: 2;
+		stroke-linecap: round;
+		stroke-linejoin: round;
+		fill: none;
+	}
 </style>
 
 <div class="component-selector">
