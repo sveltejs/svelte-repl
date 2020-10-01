d013349b3d52d8f4c6c5f75c41ed1fbacd929e70
Mario Pizzinini <marzepani@pizzinini.net>
2020-10-01 15:27:53 +0200

    remove console.clear

--- src/workers/bundler/index.js
+++ src/workers/bundler/index.js
@@ -234,7 +234,6 @@ async function get_bundle(uid, mode, cache, lookup) {
 }
 
 async function bundle({ uid, components }) {
-	console.clear();
 	console.log(`running Svelte compiler version %c${svelte.VERSION}`, 'font-weight: bold');
 
 	const lookup = {};
